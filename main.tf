1c52276050ab7a7c4431b1c6c3dfb54e29231c67
Shawn Banta <theshawnbanta@gmail.com>
2021-08-07 11:47:57 -0700

    Initial Commit

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">= 3.24.1"
     }
   }
-  required_version = "~> 0.14"
+  required_version = "> 0.14"
 }
 
 provider "aws" {
@@ -33,57 +33,68 @@ data "http" "myip" {
 }
 
 resource "aws_instance" "web_app" {
-  for_each               = aws_security_group.*.id
+  for_each               = local.security_groups
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [each.id]
+  vpc_security_group_ids = [each.value]
   user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
- tags = {
-    Name = $var.name-learn
+  tags = {
+    Name = "{$var.name}-learn-${each.key}"
   }
 }
 
+locals {
+  security_groups = {
+    sg_ping = aws_security_group.sg_ping.id,
+    sg_8080 = aws_security_group.sg_8080.id,
+  }
+}
+
+
 resource "aws_security_group" "sg_ping" {
   name = "Allow Ping"
-
-  ingress {
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.sg_8080.id]
-  }
 }
 
 resource "aws_security_group" "sg_8080" {
   name = "Allow 8080"
-
-  ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_ping.id]
-  }
 }
 
 resource "aws_security_group_rule" "allow_localhost_8080" {
-type = "ingress"
-  from_port = 8080
-  to_port = 8080                            
-  protocol = "tcp"
-  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
   security_group_id = aws_security_group.sg_8080.id
 }
 
 resource "aws_security_group_rule" "allow_localhost_ping" {
-type = "ingress"
-  from_port = -1
-  to_port = -1
-  protocol = "icmp"
-  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  type              = "ingress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
   security_group_id = aws_security_group.sg_ping.id
 }
 
+resource "aws_security_group_rule" "allow_ping" {
+  type                     = "ingress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "icmp"
+  security_group_id        = aws_security_group.sg_ping.id
+  source_security_group_id = aws_security_group.sg_8080.id
+}
+
+resource "aws_security_group_rule" "allow_8080" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg_8080.id
+  source_security_group_id = aws_security_group.sg_ping.id
+}
